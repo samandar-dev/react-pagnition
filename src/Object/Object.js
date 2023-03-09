0a637ad77682c6acdf794819aff6a2380d84c9ed
Samandar Bohodirov <samandarbohodirov@Samandars-Mac-mini.local>
2023-03-09 16:46:26 +0500

    pagnition

---
+++ src/Object/Object.js
@@ -0,0 +1,295 @@
+import img1 from "../assets/images/car1.jpeg";
+import img2 from "../assets/images/car2.jpeg";
+import img3 from "../assets/images/car3.jpeg";
+import img4 from "../assets/images/car4.jpeg";
+const itemsList = [
+  {
+    id: 1,
+    img: img1,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 2,
+    img: img2,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 3,
+    img: img3,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 4,
+    img: img4,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 5,
+    img: img1,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 6,
+    img: img2,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 7,
+    img: img3,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 8,
+    img: img4,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 9,
+    img: img1,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 10,
+    img: img2,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 11,
+    img: img3,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 12,
+    img: img4,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 13,
+    img: img1,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 14,
+    img: img2,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 15,
+    img: img3,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 16,
+    img: img4,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 17,
+    img: img1,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 18,
+    img: img2,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 19,
+    img: img3,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 20,
+    img: img4,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 21,
+    img: img1,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 22,
+    img: img2,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 23,
+    img: img3,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 24,
+    img: img4,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 1,
+    img: img1,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 2,
+    img: img2,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 3,
+    img: img3,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 4,
+    img: img4,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 5,
+    img: img1,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 6,
+    img: img2,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 7,
+    img: img3,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 8,
+    img: img4,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 9,
+    img: img1,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 10,
+    img: img2,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 11,
+    img: img3,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 12,
+    img: img4,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 13,
+    img: img1,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 14,
+    img: img2,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 15,
+    img: img3,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 16,
+    img: img4,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 17,
+    img: img1,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 18,
+    img: img2,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 19,
+    img: img3,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 20,
+    img: img4,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 21,
+    img: img1,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 22,
+    img: img2,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 23,
+    img: img3,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+  {
+    id: 24,
+    img: img4,
+    title:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, magni!",
+  },
+];
+export default itemsList;
